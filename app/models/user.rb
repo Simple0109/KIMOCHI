dd9ecd8d9355ce0b08703a550d5e1eb03c529d0a
simple0109 <p.cscscs@icloud.com>
2023-10-31 18:23:26 +0900

    roleの値を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :name, :role, presence: true
+
+  enum role: { general: 0, admin: 1 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
