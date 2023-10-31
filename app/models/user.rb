3aa108f84acc9f32acf992c5ca4bdd77e9df4e4d
simple0109 <p.cscscs@icloud.com>
2023-10-31 18:16:09 +0900

    add: Gem devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, :role, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
