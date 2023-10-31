0ac3af4417694fc2a2575fd3d12dd5b507689dd4
simple0109 <p.cscscs@icloud.com>
2023-10-31 18:44:31 +0900

    fix: spell miss(thorough->through)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   enum role: { general: 0, admin: 1 }
 
   has_many :group_users
-  has_many :groups, thorough: :group_users
+  has_many :groups, through: :group_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
