dc741118ad7e3b126513f8527f1c2f0d7731c25a
simple0109 <p.cscscs@icloud.com>
2023-11-06 14:58:39 +0900

    Fix: ログアウト後遷移先をログインページへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: [:name]
   end
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # ここを好きなパスに変更
+  end
 end
