baaf69e9741daf0b75b55482636d0e9ad6dea357
simple0109 <p.cscscs@icloud.com>
2023-11-06 14:59:24 +0900

    Add: alpinejs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import 'alpine-turbo-drive-adapter'
 import Alpine from 'alpinejs'
+
 window.Alpine = Alpine
+Alpine.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 pin "application", preload: true
-pin "alpinejs" # @3.13.2
+pin "alpinejs"
 pin "alpine-turbo-drive-adapter", to: "https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.1.0/dist/alpine-turbo-drive-adapter.esm.js"
