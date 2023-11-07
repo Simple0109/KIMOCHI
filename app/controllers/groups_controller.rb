bbfa960d08ba54d816cf94cb7ad3510be4045a37
simple0109 <p.cscscs@icloud.com>
2023-11-07 15:35:41 +0900

    Fix: groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,9 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :destroy]
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
 
 
   def index
-    @group = Group.all
+    @user_groups = current_user.groups.order(created_at: :desc)
   end
 
   def new
@@ -17,20 +17,26 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       @group.users << current_user
-      redirect_to root_path
+      redirect_to groups_path
     else
       render :new
     end
   end
 
-  def edit
-    @group.update(group_params)
-    redirect_to root_path
+  def edit;end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+       render :edit
+    end
+
   end
 
   def destroy
     @group.destroy
-    redirect_to root_path
+    redirect_to groups_path
   end
 
   private
@@ -40,7 +46,7 @@ class GroupsController < ApplicationController
   end
 
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.includes(:users).find(params[:id])
   end
 
 end
