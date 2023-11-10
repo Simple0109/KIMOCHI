416c4ca2febc1d01ea9fec0e0ec63e4957199f09
simple0109 <p.cscscs@icloud.com>
2023-11-10 17:31:36 +0900

    requestsテーブルのカラムを修正

--- db/migrate/20231108120106_create_requests.rb
+++ db/migrate/20231108120106_create_requests.rb
@@ -1,11 +1,13 @@
 class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
-      t.string :take, null: false
+      t.string :take
       t.datetime :execution_date
       t.string :image
       t.text :comment
-      t.integer :status, null: false, default: 0
+      t.integer :status
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
 
       t.timestamps
     end
