3df2bf7de179ce53474f9ba9e6a4f9cbc630963b
simple0109 <p.cscscs@icloud.com>
2023-11-16 23:42:21 +0900

    バリデーションのシンボルの位置を修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :name, presence :true, length: { maximum: 20 }
-  validates :role, presence :true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :role, presence: true
 
   enum role: { general: 0, admin: 1 }
 end
