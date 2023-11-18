758f95842fb1f3d9f8f0e64166fe8e76e4ebde9a
simple0109 <p.cscscs@icloud.com>
2023-11-18 19:53:40 +0900

    activestorageをインストール

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,14 @@
 class Profile < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
+
+  has_one_attached :avatar
+
   validates :name, presence: true, length: { maximum: 20 }
   validates :role, presence: true
+  validates :avatar, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
 
-  enum role: { general: 0, admin: 1 }
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [1200, 400]).processed
+  end
+
 end
