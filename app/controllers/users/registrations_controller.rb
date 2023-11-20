c439ef7356407f250187f3551c2a4af6b996d7fd
simple0109 <p.cscscs@icloud.com>
2023-11-20 15:02:55 +0900

    不要な記述を削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  def new
-    @user = User.new
-    @profile = @user.build_profile
-  end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
