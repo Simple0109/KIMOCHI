2c63239c2630c8f728931957106bcd8e4f1e2440
simple0109 <p.cscscs@icloud.com>
2023-12-04 15:48:44 +0900

    timezoneをTokyoに修正

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Kimochi3
   class Application < Rails::Application
+    config.time_zone = "Asia/Tokyo"
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
