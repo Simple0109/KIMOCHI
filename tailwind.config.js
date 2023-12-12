bef3bb08ec2c03a4a4364d993bc19becad840251
simple0109 <p.cscscs@icloud.com>
2023-12-12 12:40:37 +0900

    本番環境にtailwindcssteを適用

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ],
-  plugins: [require("daisyui")],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")]
 }
