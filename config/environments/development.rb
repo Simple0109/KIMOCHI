673ab12b923280b76f4e252107bd9333a76cff0b
simple0109 <p.cscscs@icloud.com>
2023-12-15 11:32:43 +0900

    開発環境でアセットにフィンガープリントが付与される問題を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Disable asset fingerprinting in development
+  config.assets.digest = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
