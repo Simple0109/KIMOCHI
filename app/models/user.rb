1051040cd177bbf9a2e3f37da1b587cff00d4e88
simple0109 <p.cscscs@icloud.com>
2024-01-04 13:49:58 +0900

    lineプロフィール画像がprofile.avatarにアタッチされる機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+require 'mimemagic'
+require 'open-uri'
+
 class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 7 }
@@ -31,8 +34,30 @@ class User < ApplicationRecord
     access_token = credentials['refresh_token']
     access_secret = credentials['secret']
     credentials = credentials.to_json
+  end
+
+  def set_profile_name(url)
     profile = self.profile || build_profile
     profile.name = info['name']
+    profile.save!
+  end
+
+  def set_profile_avatar(url)
+    profile = self.profile || build_profile
+    downloaded_avatar = URI.open(url)
+
+    mimetype = MimeMagic.by_magic(downloaded_avatar)
+    ext = case mimetype.type
+          when "image/jpeg"
+            "jpg"
+          when "image/png"
+            "png"
+          else
+            "unknown"
+          end
+
+    filename = "user_#{user.id}_avatar.#{exe}"
+    profile.avatar.attach(io: downloaded_avatar, filename: filename, content_type: mime_type.type)
     profile.save!
   end
 
