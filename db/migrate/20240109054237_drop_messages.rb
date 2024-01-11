d601a1de53016c0cfa56a5e197ba118c57bc6c8f
simple0109 <p.cscscs@icloud.com>
2024-01-11 18:34:37 +0900

    誤ったマイグレーションファイルを削除

--- db/migrate/20240109054237_drop_messages.rb
+++
@@ -1,9 +0,0 @@
-class DropMessages < ActiveRecord::Migration[7.0]  # バージョンは適宜調整してください
-  def up
-    drop_table :messages
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
