4aa1c3f02e9ad21380d3713c3bc4a4954d05a1c8
simple0109 <p.cscscs@icloud.com>
2024-01-11 18:03:22 +0900

    actioncableによるリアルタイムチャット機能実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,15 @@ import { Application } from "stimulus";
 
 // 個々のStimulusコントローラーをインポート
 import ImagePreviewController from "./controllers/image_preview_controller";
+import ChatController from "./controllers/chat_controller";
+
 
 // Stimulusのアプリケーションインスタンスを作成
 const application = Application.start();
 
 // コントローラーをStimulusアプリケーションに登録
 application.register("image-preview", ImagePreviewController);
+application.register("chat", ChatController);
+
 
 // これ以降に、他のJavaScriptやライブラリのセットアップを行う
