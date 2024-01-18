50d74d55756fc4e202c25d5d12863831cf8b1781
simple0109 <p.cscscs@icloud.com>
2024-01-18 12:08:50 +0900

    ヘッダー、サイドバーのアクティブ化実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def sidebar_link_class(group)
+    if (params[:controller] == 'groups' && ['show', 'edit'].include?(params[:action]) && params[:id] == group.id.to_s) ||
+       (params[:controller] == 'requests' && ['index', 'show', 'new', 'edit'].include?(params[:action]) && params[:group_id] == group.id.to_s)
+      'text-blue-500'
+    else
+      'text-gray-500'
+    end
+  end
+
+  def header_group_new_class
+    if params[:controller] == 'groups' && params[:action] == 'new'
+      'text-blue-500'
+    else
+      'text-gray-500'
+    end
+  end
+
+  def header_group_index_class
+    if params[:controller] == 'groups' && params[:action] == 'index'
+      'text-blue-500'
+    else
+      'text-gray-500'
+    end
+  end
+
+  def header_profile_class
+    if params[:controller] == 'profiles' && ['show', 'edit'].include?(params[:action])
+      'text-blue-500'
+    else
+      'text-gray-500'
+    end
+  end
 end
