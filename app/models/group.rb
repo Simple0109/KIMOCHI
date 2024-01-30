395048af21f977163f33ee20fc07fdfc610936e5
simple0109 <p.cscscs@icloud.com>
2024-01-31 00:15:45 +0900

    groupsテーブルにuser_idをreference型で追加したことによるアソシエーション設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,6 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
 
   has_many :requests, dependent: :destroy
+
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id', inverse_of: :owned_groups
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
 
   has_many :messages, dependent: :destroy
 
+  has_many :owned_groups, class_name: 'Group', inverse_of: :owner, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
