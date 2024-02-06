b10f795e7be6abc3da6665fab95550c0c9a6b50d
simple0109 <p.cscscs@icloud.com>
2024-02-06 11:54:37 +0900

    コメントアウトの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_out_path_for(_resource_or_scope)
-    root_path # ここを好きなパスに変更
+    root_path
   end
 
   def after_sign_in_path_for(resource)
--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,28 +1,17 @@
-// stimulusのControllerクラスをインポートすることを宣言
 import { Controller } from "stimulus"
-// 他のファイルからこのコントローラをインポートできるようにすることを宣言
 export default class extends Controller {
-  // コントローラが操作するDOM要素を定義。これによりコントローラ内でthis.inputTargetとthis.previewTargetを使用して特定のDOM要素を参照できる
   static targets = ["input", "preview"]
 
-  //previewImageメソッドを定義
   previewImage() {
-    // 作成しているコントローラ内で使用するためにinput, preview, targetを定義
     const input = this.inputTarget
     const preview = this.previewTarget
-    // input.filesはユーザーが選択したファイルのリスト
     const files = input.files
     const width = this.data.get('width')
 
 
-    // files[0]は最初のファイルを参照する
     if (files && files[0]) {
-      // readerをFileReaderオブジェクトとして定義
       const reader = new FileReader()
-      // 画像が読み込まれる(e)とreader.onloadイベントが発火する
-      // reader.onloadは「ファイルの読み込みが完了したら次に何をするか」という指示をするもの
       reader.onload = (e) => {
-        // 発火する内容を定義
         preview.innerHTML = `<img src="${e.target.result}" style="max-width: ${width}px;">`;
       }
       reader.readAsDataURL(files[0])
