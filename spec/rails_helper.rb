0d59ab4e34dff134ad332029d8517597841691a5
simple0109 <p.cscscs@icloud.com>
2024-02-16 19:14:47 +0900

    特定のテストを行えるよう設定追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.filter_run_when_matching :focus
   config.before(:each, type: :system) do
     driven_by(:selenium_chrome_headless)
     # driven_by(:selenium_chrome)
