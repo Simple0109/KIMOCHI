d289e5944693f80f62ad43665205e8e7ed2b1a12
simple0109 <p.cscscs@icloud.com>
2024-02-16 18:08:08 +0900

    ログイン処理をモジュール化。それに伴いomniauth.rbを削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,5 +65,6 @@ RSpec.configure do |config|
     driven_by(:selenium_chrome_headless)
     # driven_by(:selenium_chrome)
     # driven_by(:rack_test)
+    config.include AuthHelpers, type: :system
   end
 end
