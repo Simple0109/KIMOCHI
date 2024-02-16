0dc0455956a2d5893eb8ca352c96ca036e37987c
simple0109 <p.cscscs@icloud.com>
2024-02-16 18:08:27 +0900

    ログアウトテスト追加

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -3,14 +3,26 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   describe 'ログイン' do
     before do
-      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
-      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:line]
+      user_sign_in_via_line
     end
     context 'LINEログインボタンを押した場合' do
       it 'ログインができる' do
         visit root_path
         click_on 'LINEアカウントでログイン'
         expect(page).to have_content 'マイページ'
+      end
+    end
+  end
+
+  describe 'ログアウト', type: :system do
+    context 'ログアウトボタンを押した場合' do
+      it 'ログアウトが成功する' do
+        user_sign_in_via_line
+        visit root_path
+        click_on 'LINEアカウントでログイン'
+        click_on 'ログアウト'
+        accept_alert 'ログアウトしますか？'
+        expect(page).to have_content 'LINEでログイン'
       end
     end
   end
