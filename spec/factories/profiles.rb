4c1bc792c07fafe0d7159b9b488842409a7e2900
simple0109 <p.cscscs@icloud.com>
2024-03-05 22:09:21 +0900

    スペース追加

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :profile do
-    name { Faker::Internet.username}
+    name { Faker::Internet.username }
     role { 0 }
-    association:user
+    association :user
 
     after(:build) do |profile|
-
       profile.avatar.attach(
         io: File.open(Rails.root.join('spec', 'fixtures', 'test.png')),
         filename: 'test.png',
