566316a74f97e528ffd868ae79fbe2f1eb690e58
simple0109 <p.cscscs@icloud.com>
2024-06-09 19:02:26 +0900

    Groupのmodel spec修正

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -19,28 +19,29 @@ RSpec.describe Group, type: :model do
         expect(group).not_to be_valid
       end
     end
+  end
 
-    describe 'クラスメソッドのテスト' do
-      describe 'owner?メソッド' do
-        it 'ユーザーがグループのオーナーの場合、trueを返す' do
-          user = create(:user)
-          group = create(:group, owner: user)
-          expect(group.owner?(user)).to be true
-        end
-        it 'ユーザーがグループのオーナーでない場合、falseを返す' do
-          other_user = create(:user)
-          owner = create(:user)
-          group = create(:group, owner: owner)
-          expect(group.owner?(other_user)).to be false
-        end
+  describe 'クラスメソッドのテスト' do
+    context 'owner?メソッド' do
+      it 'ユーザーがグループのオーナーの場合、trueを返す' do
+        user = create(:user)
+        group = create(:group, owner: user)
+        expect(group.owner?(user)).to be true
+      end
+
+      it 'ユーザーがグループのオーナーでない場合、falseを返す' do
+        other_user = create(:user)
+        owner = create(:user)
+        group = create(:group, owner: owner)
+        expect(group.owner?(other_user)).to be false
       end
     end
+  end
 
-    describe 'アソシエーションのテスト' do
-      it { should have_many(:group_users).dependent(:destroy) }
-      it { should have_many(:users).through(:group_users) }
-      it { should have_many(:requests).dependent(:destroy) }
-      it { should belong_to(:owner).class_name('User').with_foreign_key('user_id').inverse_of(:owned_groups) }
-    end
+  describe 'アソシエーションのテスト' do
+    it { should have_many(:group_users).dependent(:destroy) }
+    it { should have_many(:users).through(:group_users) }
+    it { should have_many(:requests).dependent(:destroy) }
+    it { should belong_to(:owner).class_name('User').with_foreign_key('user_id').inverse_of(:owned_groups) }
   end
 end
