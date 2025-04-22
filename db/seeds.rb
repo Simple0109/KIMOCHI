e19fb8340ea05987fa31f078ee3100e977092113
Simple0109 <p.cscscs@icloud.com>
2025-04-22 23:27:50 +0900

    seeederを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 User.destroy_all
 Profile.destroy_all
 
-10.times do |i|
+5.times do |i|
   user = User.create!(
     email: "test#{ i + 1 }@example.com",
     password: "password",
